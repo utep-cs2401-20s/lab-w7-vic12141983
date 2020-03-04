22c245e56c808820b42009624f0cef63f9e1e323
victor <vherreravi@miners.uteo.edu>
2020-03-04 14:50:06 -0700

    Lab 7 alsmot i think

--- Java/src/SortOfSort.java
+++ Java/src/SortOfSort.java
@@ -1,26 +1,41 @@
 public class SortOfSort {
     public static void sortOfSort(int[] arr) {
-        int max =0;
-        int min =0;
-        for (int i = 0; i < arr.length-1; i++) {
-            if(arr[i]>max){
-                max = arr[i];
-                if(arr[i]<max){
-                    min =arr[i];
+        int max = arr[0];
+        int min = arr[0];
+        int counter = 0;
+        int large = arr.length-1;
 
-                }
 
+        for( int i =0;i<arr.length;i++) {
+            if(arr[i]>max&&counter>2){
+                max=arr[i];
+              int temp = arr[large];
+              arr[i] = arr[large];
+              arr[large] = temp;
+              large = large -1;
+                System.out.print(max);
+              counter++;
 
             }
 
-
         }
+
     }
+
+
     public static void main(String[] args) {
-        int n[] = {2,7,1,3,0,9,6,5};
+        int n[] = {2, 7, 1, 3, 0, 9, 6, 5};
         sortOfSort(n);
+       //printStuff(n);
 
 
+    }
+
+    public static void printStuff(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
 
     }
 }
+
