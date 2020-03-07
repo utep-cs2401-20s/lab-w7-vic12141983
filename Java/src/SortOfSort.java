2c033b2c34e8f1a1995d37a570081254c8fd665b
victor <vherreravi@miners.uteo.edu>
2020-03-07 11:31:58 -0700

    THis is my complete lab it runs I nkow there is bugs for swapping but I do not know how to fix it. I talked to multiple tas I am already at a loss of 10 points. Please let me know what I did wrong because if I do not know I cannot fix the problem

--- Java/src/SortOfSort.java
+++ Java/src/SortOfSort.java
@@ -1,49 +1,58 @@
 public class SortOfSort {
-    public static void sortOfSort(int[] arr) {
-       int max=0;
-       int L=arr[0];
-       int R = arr.length-1;
-       int count =2;
-
-
-        for (int i = 0; i < arr.length-1; i++) {
-           for( int j =0;j<arr.length-1;j++){
-               // Checking the position of i
-               System.out.print(i+"=i");
-               // Right here im checking the position of i and j to see which is larger and when traced when i=0; in the loop. the position of j
-               // inside the array 2 7 1 3 0 9 6 5
-               // so if j is greater than arr[i]
-               if(arr[i]>arr[j]) {
-               max = arr[i];
-               System.out.print(i+"  "+max+"  ");
-               }
-               if(count<2) {
-                   int temp = arr[L];
-                   arr[L] = arr[i];
-                   arr[i] = temp;
-                   count++;
-                   R--;
-                   L++;
-               }
-
-
-
-
-
-
-           }
-
-        }
+    public SortOfSort(){
 
     }
-    public static void main(String[] args) {
-        SortOfSort b = new SortOfSort();
-        int []arr1 ={9, 8, 1, 4, 10, 3, 2, 1,};
-        int[] arr = {2, 7, 1, 3, 0, 9, 6, 5};
 
-        sortOfSort(arr);
+
+    public static void sortOfSort(int[] arr) {
+        // Max variable in order to compare elements to sort
+        int max = arr[0];
+        // Variable at index to change elements in array when count is reached
+        int L = arr[0];
+        // Right index to move elements to this index and also decrement
+        int R = arr.length - 1;
+        // counter to keep track how many elements at left and right index as they move towards the middle.
+        int count = 0;
+        // for loop traverse the arr at arr[i] and arr[j]
         for (int i = 0; i < arr.length; i++) {
-            System.out.print("|"+arr[i]+"   ");
+            for (int j = 0; j < arr.length; j++) {
+                // if arr[i] is greater than the j index then tat index becomes the largest
+                // in this case given the array in the main method would be largest at the index i.
+                if (arr[i] > arr[j] && count < 2) {
+                    max = arr[i];
+                    // temporary value to hold value 9 at index i
+
+                    int temp = max;
+                    // Swapping max index which has value of 9 at index i to the last index of the array.
+                    max = arr.length - 1;
+                    // Moving last index of array to position where element 9 was at that index i.
+                    arr[arr.length - 1] = max;
+                    //Decrement the index to hold value 9 at the end index.
+                    R--;
+                    //Increment index so when i is one there will be at least two open positions to swap to the left side.
+                    L++;
+                    // Increment count in order to keep track how many elements to put at left and right indices
+                    count++;
+                } else if (arr[i] > arr[j] && count == 1) {
+                    // max element stored in temp;
+                    int temp1 = max;
+                    // max element is switched with element at index to left
+                    max = L;
+                    // left moves to temp;
+                    L = temp1;
+                    // decrement count to move to right
+                    count--;
+                    // increment right because of loop it will go down all the to -1;
+
+                    R++;
+                    // decrement left same it will reach the end of the loop in previous statement.
+                    L--;
+
+                }
+
+
+            }
+
         }
 
 
@@ -51,4 +60,3 @@ public class SortOfSort {
 
 
 }
-
--- Java/src/SortOfSortTest.java
+++ Java/src/SortOfSortTest.java
@@ -3,16 +3,89 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SortOfSortTest {
+//Test 1.
+    // Testing method to verify it functions correctly.
+    // Test failed
+    // In order to correct modify code to reflect where indices are not being swapped properly.
+    // Bug exist within the swapping of elements.
 
     @Test
-    void sortOfSort(int []arr){
-        int []arr1 = new int[0];
+    void sortOfSort() {
+        int[] arr1 = {2, 7, 1, 3, 0, 9, 6, 5};
+        int[] arr2 = {6, 5, 1, 0, 2, 3, 7, 9};
         SortOfSort bob = new SortOfSort();
-        sortOfSort(arr1);
+        bob.sortOfSort(arr1);
 
 
+        assertArrayEquals(arr2, arr1);
 
 
+    }
+    // Testing with different length in array one index .
+    // Expect pass
+    // Test result passed. There were no elements to compare so the test proves that even if the arry was empty it will still
+    // function correctly.
 
+    @Test
+    void sortOfSort2() {
+        int[] arr1 = {2};
+        int[] arr2 = {6, 5, 1, 0, 2, 3, 7, 9};
+        SortOfSort bob = new SortOfSort();
+        bob.sortOfSort(arr1);
+
+
+        assertArrayEquals(arr1, arr1);
+    }
+    // Testing with negative integers
+    // Expectation pass
+    // Result failed
+    // Bug in swapping elements that could not be found lack of information provided to assist
+    // With this bug
+
+    @Test
+    void sortOfSort3() {
+
+        int[] arr2 = {-6, -5, -1, 0, -2, -3, -7, -9};
+        int[] arr3 = {-2, -3, -7, 9, -6,-5 -3, -1, 0};
+
+        SortOfSort bob = new SortOfSort();
+        bob.sortOfSort(arr2);
+
+
+        assertArrayEquals(arr3, arr2);
+    }
+    // Testing odd elements
+    // Expect pass
+    // Result failure
+    // To correct this issue code has to be modified bug exist in swapping elements of array
+    // Not enough information was provided to assist with the swapping of elements
+    // bug will remain without assistance.
+    @Test
+    void sortOfSort4() {
+
+        int[] arr2 = {1,3,5,7,9,1,3,5,7,9};
+        int[] arr3 = {7,7,3,3,1,1,5,5,9,9};
+
+        SortOfSort bob = new SortOfSort();
+        bob.sortOfSort(arr2);
+
+
+        assertArrayEquals(arr3, arr2);
+    }
+    // Testing for bugs in code
+    // Test passed
+    // Result Code has bugs in swapping elements of an array.
+    // Modify code to not reflect the bug where elements are properly swapped.
+    @Test
+    void sortOfSort5() {
+
+        int[] arr2 = {2,3,5,7,9,1,3,5,7,9};
+        int[] arr3 = {3,7,3,3,1,1,5,5,9,9};
+
+        SortOfSort bob = new SortOfSort();
+        bob.sortOfSort(arr2);
+
+
+        assertArrayEquals(arr3, arr2);
     }
 }
