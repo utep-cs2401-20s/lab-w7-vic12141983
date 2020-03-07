8842f643e4bc65374bc120939d25c5e5ad5e5a72
victor <vherreravi@miners.uteo.edu>
2020-03-06 23:50:58 -0700

    I have tried all week to do this method. multiple ways I looked it up i found a solution but its ridiculous. I do not understand the concept of swapping the array I know when I trace it, following it works when I use it in java it does not work. I am not going to submit my completed lab today. But i I will push what i have I modified the mehtod from the last push. Still confused IM actually really frustrated. with this. I know how it works but im  missing somthing.

---
+++ Java/src/SortOfSortTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortOfSortTest {
+
+    @Test
+    void sortOfSort(int []arr){
+        int []arr1 = new int[0];
+        SortOfSort bob = new SortOfSort();
+        sortOfSort(arr1);
+
+
+
+
+
+    }
+}
