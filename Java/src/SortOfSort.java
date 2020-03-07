c169620279871344aa7009fe44b31e8219b6098b
victor <vherreravi@miners.uteo.edu>
2020-03-06 23:03:47 -0700

    I have tried all week to do this method. multiple ways I looked it up i found a solution but its ridiculous. I do not understand the concept of swapping the array I know when I trace it, following it works when I use it in java it does not work. I am not going to submit my completed lab today. But i I will push what i have I modified the mehtod from the last push.

--- Java/src/SortOfSort.java
+++ Java/src/SortOfSort.java
@@ -1,41 +1,51 @@
 public class SortOfSort {
     public static void sortOfSort(int[] arr) {
-        int max = arr[0];
-        int min = arr[0];
-        int counter = 0;
-        int large = arr.length-1;
+       int max=0;
+       int L=arr[0];
+       int R = arr.length-1;
+       int count =2;
 
 
-        for( int i =0;i<arr.length;i++) {
-            if(arr[i]>max&&counter>2){
-                max=arr[i];
-              int temp = arr[large];
-              arr[i] = arr[large];
-              arr[large] = temp;
-              large = large -1;
-                System.out.print(max);
-              counter++;
+        for (int i = 0; i < arr.length-1; i++) {
+           for( int j =0;j<arr.length-1;j++){
+               if(arr[j]>arr[i]&&count==0) {
+                   max = arr[j];
+               }
+                   //System.out.print(i+"  ");
+                   int temp = arr[i];
+                   arr[i]=arr[j];
+                   arr[j]=temp;
+                  arr[arr.length-1]=arr[arr.length-2];
+                  count++;
+                  R--;
+                 if(count<2){
+                     int temp1 =arr[i];
+                     arr[i] = arr[L];
+                     arr[L] = temp1;
 
-            }
+               }
+
+
+
+
+           }
 
         }
 
     }
-
-
     public static void main(String[] args) {
-        int n[] = {2, 7, 1, 3, 0, 9, 6, 5};
-        sortOfSort(n);
-       //printStuff(n);
+        SortOfSort b = new SortOfSort();
+        int []arr1 ={9, 8, 1, 4, 10, 3, 2, 1,};
+        int[] arr = {2, 7, 1, 3, 0, 9, 6, 5};
 
-
-    }
-
-    public static void printStuff(int[] arr) {
+        sortOfSort(arr);
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+            System.out.print("|"+arr[i]+"   ");
         }
 
+
     }
+
+
 }
 
